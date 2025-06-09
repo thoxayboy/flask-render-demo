321ab8455b9f9881d41f0eb5b15a1cd50b7249e2
HUY HOANG <HUY HOANG@DESKTOP-ES2DGG1>
2025-06-09 19:14:39 +0700

    Fix app to use dynamic port from environment variable

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Xin chào từ Flask chạy trên Render!"
 
-if __name__ == '__main__':
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # lấy port từ biến môi trường
+    app.run(host="0.0.0.0", port=port)
